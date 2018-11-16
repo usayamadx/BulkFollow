d74411f0bc23457f6e7342aa87181f2d41f13df9
usayamadx <u@usayama.com>
2018-11-17 00:47:22 +0900

    to short codes

--- README.md
+++ README.md
@@ -9,7 +9,24 @@
 
 ### install
 ```
+pip install requests requests_oauthlib
 pip show oauth2
+```
+
+### create config.ini
+
+```
+touch config.ini
+vi config.ini
+```
+
+set
+```
+[settings]
+API_KEY = "Your API key"
+API_SECRET_KEY = "Your API secret key"
+ACCESS_TOKEN = "Your Access token"
+ACCESS_TOKEN_SECRET = "Your Access token secret"
 ```
 
 ### get access token
--- bulkFollow.py
+++ bulkFollow.py
@@ -1,23 +1,16 @@
-import sys
-import configparser
-import oauth2 as oauth
-import json
+import json, config
+from requests_oauthlib import OAuth1Session
 
-# CONFIG
-config = configparser.ConfigParser()
-config.read('./config.ini', 'UTF-8')
-API_KEY = config.get('settings', 'API_KEY')
-API_SECRET_KEY = config.get('settings', 'API_SECRET_KEY')
-ACCESS_TOKEN = config.get('settings', 'ACCESS_TOKEN')
-ACCESS_TOKEN_SECRET = config.get('settings', 'ACCESS_TOKEN_SECRET')
-ENDPOINT = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+twitter = OAuth1Session(config.API_KEY, config.API_SECRET_KEY, config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
+url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
+params = {'count' : 5}
+res = twitter.get(url, params = params)
 
-
-api = oauth.Consumer(key = API_KEY, secret = API_SECRET_KEY)
-access_token = oauth.Token(key = ACCESS_TOKEN, secret = ACCESS_TOKEN_SECRET)
-client = oauth.Client(api, access_token)
-data = client.request(ENDPOINT)
-tweets = json.loads(data)
-
-for tweet in tweets:
-    print(tweet['text'])
+if res.status_code == 200:
+    timelines = json.loads(res.text)
+    for line in timelines:
+        print(line['user']['name']+'::'+line['text'])
+        print(line['created_at'])
+        print('*******************************************')
+else:
+    print("Failed: %d" % res.status_code)
